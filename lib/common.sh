a9161a5038cbcecafd7fe8933a3592e99ff3828c
Alex Mounka <alex.mounka@ginov.io>
2025-10-05 20:02:14 +0100

    Correction du buildpack Tempo pour Scalingo

--- lib/common.sh
+++ lib/common.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
+# Fonction pour l'indentation du texte
+indent() {
+  # Version portable de l'indentation
+  # Utilise sed pour ajouter 7 espaces au début de chaque ligne
+  sed -u 's/^/       /'
+}
+
+# Fonction pour exporter les variables d'environnement
 export_env_dir() {
   local env_dir=$1
   if [ -d "$env_dir" ]; then
-    for e in $(ls $env_dir);
-    do
-      echo "  $e=$(cat $env_dir/$e)"
-      export $e=$(cat $env_dir/$e)
+    local whitelist_regex=${2:-''}
+    local blacklist_regex=${3:-'^(PATH|GIT_DIR|CPATH|CPPATH|LD_PRELOAD|LIBRARY_PATH)$'}
+    
+    if [ -z "$env_dir" ]; then
+      return
+    fi
+    
+    for e in $(ls $env_dir); do
+      echo "$e" | grep -E "$whitelist_regex" | grep -qvE "$blacklist_regex" &&
+      export "$e=$(cat $env_dir/$e)"
+      :
     done
   fi
 }
-
